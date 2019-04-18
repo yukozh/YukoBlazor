88920e234e3ff99736535bb2bdcf6b04264b3020
Yuko Zheng <1@1234.sh>
2019-04-19 00:53:32 +0800

    Test main

---
+++ test/YukoBlazor.Server.Tests/LaunchCommandLineTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace YukoBlazor.Server.Tests
+{
+    public class LaunchCommandLineTests
+    {
+        [Fact]
+        public void LaunchFailedWithInvalidArgumentsTest()
+        {
+            Assert.Throws<IndexOutOfRangeException>(
+                () => Program.Main(new[] { "-db" }));
+        }
+    }
+}
